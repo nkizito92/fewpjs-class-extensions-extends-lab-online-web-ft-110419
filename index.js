dfb9083a86d4a5657d8267e31e5e180bbef5c004
Kizito Njoku <nkizito57@gmail.com>
2020-03-18 01:36:55 +0000

    Done.

--- index.js
+++ index.js
@@ -10,9 +10,36 @@ class Polygon {
   
   get perimeter(){
     let total = 0;
-    for(let i = 0; (this.side).length; i++){
+    for(let i = 0; i < this.countSides; i++){
       total += (this.side)[i];
     }
       return total
+  }
+
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+     if (this.countSides !== 3) return 
+      let side1 = this.side[0]
+    let side2 = this.side[1]
+    let side3 = this.side[2]
+    return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+     
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if(this.countSides !== 4) return 
+    let side1 = this.side[0]
+    let side2 = this.side[1]
+    let side3 = this.side[2]
+    let side4 = this.side[3]
+    return ((side1 === side2) && (side1 === side3) && (side1 === side4))
+     
+  }
+  get area() {
+    return (this.side)[0]**2
   }
 }
