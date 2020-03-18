570992b3366c6933d1f6773a470c546ad0ad91bf
Kizito Njoku <nkizito57@gmail.com>
2020-03-18 01:18:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+class Polygon {
+ constructor(side) {
+    this.side = side;
+  }
+  
+  get countSides() {
+    return (this.side).length
+  }
+  
+  get perimeter(){
+    let total = 0;
+    for(let i = 0; (this.side).length; i++){
+      total += (this.side)[i];
+    }
+      return total
+  }
+}
